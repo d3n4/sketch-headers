bbae4a9342f0aa7326ab52cec15d596889d7ddce
mathieudutour <mathieu@dutour.me>
2019-07-11 14:12:47 +0200

    fix script

--- deploy.js
+++ deploy.js
@@ -3,8 +3,8 @@
 // usage: npm run deploy -- --sketchVersion=48-beta2
 
 const fs = require('fs')
-const {argv} = require('yargs')
-const {exec} = require('child-process-promise')
+const { argv } = require('yargs')
+const { execSync } = require('child_process')
 
 const version = argv.sketchVersion
 
@@ -24,16 +24,12 @@ const endOfReadme = readme.slice(startOfTable)
 let previousVersion = (new RegExp(/\| \[(.*?)\]/g)).exec(readme.slice(startOfTable))
 previousVersion = previousVersion ? previousVersion[1] : previousVersion
 
+require('./lib')()
+
 fs.writeFileSync(
   './README.md',
   startOfReadme + `\n| [${version}](https://github.com/skpm/sketch-headers/tree/${version}) | ${previousVersion ? `[${previousVersion}...${version}](https://github.com/skpm/sketch-headers/compare/${previousVersion}...${version})` : 'X'} | ${previousVersion ? `[${previousVersion}...${version}](https://github.com/skpm/sketch-headers/compare/${previousVersion}...${version}.diff)` : 'X'} |` + endOfReadme,
   {encoding: 'utf8'})
 
-require('./lib')(argv)
-.then(() => {
-  return exec(`git add . && git commit -m "version ${version}" && git tag ${version} && git push && git push origin ${version} && cp -r headers .temp-headers && git checkout gh-pages && cp -r .temp-headers ${version} && rm -rf latest && cp -r .temp-headers latest && git add . && git commit -m "version ${version}" && git push && git checkout master && rm -rf .temp-headers`, {maxBuffer: 1024 * 3000})
-})
-.then(() => console.log('done'))
-.catch(console.error)
-
-
+execSync(`git add . && git commit -m "version ${version}" && git tag ${version} && git push && git push origin ${version} && cp -r headers .temp-headers && git checkout gh-pages && cp -r .temp-headers ${version} && rm -rf latest && cp -r .temp-headers latest && git add . && git commit -m "version ${version}" && git push && git checkout master && rm -rf .temp-headers`, {maxBuffer: 1024 * 3000})
+console.log('done')
